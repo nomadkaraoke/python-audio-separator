ntamotsu/fork_python-audio-separator

perf: optimize PyTorch stem separation
- eliminate redundant RoFormer tail chunks and reuse consecutive model loads

- keep supported MPS spectral work and bounded accumulators on-device

- add observable precision modes and regional compilation with safe fallbacks

- preserve float32 numerical islands and scaled BS-RoFormer attention

audio_separator/separator/architectures/demucs_separator.py
@@ -8,6 +8,7 @@ from audio_separator.separator.uvr_lib_v5.demucs.apply import apply_model, demuc
 from audio_separator.separator.uvr_lib_v5.demucs.hdemucs import HDemucs
 from audio_separator.separator.uvr_lib_v5.demucs.pretrained import get_model as get_demucs_model
 from audio_separator.separator.uvr_lib_v5 import spec_utils
+from audio_separator.separator.uvr_lib_v5.device_utils import should_accumulate_on_device
 
 DEMUCS_4_SOURCE = ["drums", "bass", "other", "vocals"]
 
@@ -21,6 +22,17 @@ DEMUCS_6_SOURCE_MAPPER = {
     CommonSeparator.GUITAR_STEM: 4,
     CommonSeparator.PIANO_STEM: 5,
 }
+
+
+def _estimate_demucs_full_track_buffer_bytes(channels, samples, num_sources, shifts, num_bag_models):
+    """Estimate peak float32 storage retained across Demucs split and shift passes."""
+    input_bytes = channels * samples * 4
+    output_copies = 2 if shifts > 1 else 1
+    if num_bag_models:
+        output_copies = max(output_copies, 2)
+        if num_bag_models > 1 and shifts > 1:
+            output_copies = 3
+    return 3 * input_bytes + output_copies * num_sources * input_bytes + samples * 4
 
 
 class DemucsSeparator(CommonSeparator):
@@ -107,19 +119,34 @@ class DemucsSeparator(CommonSeparator):
 
         self.logger.debug("Loading model for demixing...")
 
-        self.demucs_model_instance = HDemucs(sources=DEMUCS_4_SOURCE)
-        self.demucs_model_instance = get_demucs_model(name=os.path.splitext(os.path.basename(self.model_path))[0], repo=Path(os.path.dirname(self.model_path)))
-        self.demucs_model_instance = demucs_segments(self.segment_size, self.demucs_model_instance)
-        self.demucs_model_instance.to(self.torch_device)
-        self.demucs_model_instance.eval()
+        separation_failed = False
+        try:
+            self.demucs_model_instance = HDemucs(sources=DEMUCS_4_SOURCE)
+            self.demucs_model_instance = get_demucs_model(name=os.path.splitext(os.path.basename(self.model_path))[0], repo=Path(os.path.dirname(self.model_path)))
+            self.demucs_model_instance = demucs_segments(self.segment_size, self.demucs_model_instance)
+            self.demucs_model_instance.to(self.torch_device)
+            self.demucs_model_instance.eval()
 
-        self.logger.debug("Model loaded and set to evaluation mode.")
+            self.logger.debug("Model loaded and set to evaluation mode.")
 
-        source = self.demix_demucs(mix)
-
-        del self.demucs_model_instance
-        self.clear_gpu_cache()
-        self.logger.debug("Model and GPU cache cleared after demixing.")
+            source = self.demix_demucs(mix)
+        except BaseException:
+            separation_failed = True
+            raise
+        finally:
+            if hasattr(self, "demucs_model_instance"):
+                del self.demucs_model_instance
+            try:
+                self.clear_gpu_cache()
+            except Exception as cleanup_error:
+                if not separation_failed:
+                    raise
+                self.logger.warning(
+                    "Failed to clear the GPU cache after Demucs inference failed: %s",
+                    cleanup_error,
+                    exc_info=True,
+                )
+            self.logger.debug("Model and GPU cache cleared after demixing.")
 
         output_files = []
         self.logger.debug("Processing output files...")
@@ -166,9 +193,26 @@ class DemucsSeparator(CommonSeparator):
         self.logger.debug("Starting demixing process in demix_demucs...")
 
         processed = {}
-        mix = torch.tensor(mix, dtype=torch.float32)
+        num_sources = len(self.demucs_model_instance.sources)
+        estimated_buffer_bytes = _estimate_demucs_full_track_buffer_bytes(
+            channels=mix.shape[0],
+            samples=mix.shape[-1],
+            num_sources=num_sources,
+            shifts=self.shifts,
+            num_bag_models=len(getattr(self.demucs_model_instance, "models", ())),
+        )
+        accumulate_on_device = should_accumulate_on_device(self.torch_device, estimated_buffer_bytes)
+        mix_device = self.torch_device if accumulate_on_device else torch.device("cpu")
+        if self.torch_device.type == "mps" and not accumulate_on_device:
+            self.logger.info(
+                "Keeping full-track Demucs buffers on CPU for this long input to limit MPS memory use "
+                f"(estimated buffers: {estimated_buffer_bytes / 1024**3:.2f} GiB)."
+            )
+        mix = torch.tensor(mix, dtype=torch.float32, device=mix_device)
         ref = mix.mean(0)
-        mix = (mix - ref.mean()) / ref.std()
+        ref_mean = ref.mean()
+        ref_std = ref.std()
+        mix.sub_(ref_mean).div_(ref_std)
         mix_infer = mix
 
         with torch.no_grad():
@@ -185,7 +229,8 @@ class DemucsSeparator(CommonSeparator):
                 progress=True,
             )[0]
 
-        sources = (sources * ref.std() + ref.mean()).cpu().numpy()
+        sources.mul_(ref_std).add_(ref_mean)
+        sources = sources.cpu().numpy()
         sources[[0, 1]] = sources[[1, 0]]
         processed[mix] = sources[:, :, 0:None].copy()
         sources = list(processed.values())

audio_separator/separator/separator.py
@@ -24,6 +24,7 @@ import torch.amp.autocast_mode as autocast_mode
 import onnxruntime as ort
 from tqdm import tqdm
 from audio_separator.separator.ensembler import Ensembler
+from audio_separator.separator.execution_policy import AUTOCAST, FP32, NATIVE_FP16
 
 # Mapping of common stem name variations to canonical names for ensemble grouping.
 STEM_NAME_MAP = {
@@ -73,7 +74,9 @@ class Separator:
         invert_using_spec (bool): Flag to invert using spectrogram.
         sample_rate (int): The sample rate of the audio.
         use_soundfile (bool): Use soundfile for audio writing, can solve OOM issues.
-        use_autocast (bool): Flag to use PyTorch autocast for faster inference.
+        use_autocast (bool): Use PyTorch autocast when the loaded model and device support it.
+        use_torch_compile (bool): Compile verified repeated model blocks when supported.
+        use_native_fp16 (bool): Convert a verified model to native float16 inference when supported.
 
     MDX Architecture Specific Attributes:
         hop_length (int): The hop length for STFT.
@@ -130,8 +133,13 @@ class Separator:
         ensemble_weights=None,
         ensemble_preset=None,
         info_only=False,
+        use_torch_compile=False,
+        use_native_fp16=False,
     ):
         """Initialize the separator."""
+        if use_autocast and use_native_fp16:
+            raise ValueError("use_autocast and use_native_fp16 are mutually exclusive precision modes.")
+
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
         self.log_level = log_level
@@ -211,6 +219,8 @@ class Separator:
 
         self.use_soundfile = use_soundfile
         self.use_autocast = use_autocast
+        self.use_native_fp16 = use_native_fp16
+        self.use_torch_compile = use_torch_compile
         self.use_directml = use_directml
 
         self.chunk_duration = chunk_duration
@@ -249,12 +259,27 @@ class Separator:
         self.model_instance = None
         self.model_filename = None
         self.model_filenames = []
+        self._loaded_model_filename = None
 
         self.model_is_uvr_vip = False
         self.model_friendly_name = None
 
         if not info_only:
             self.setup_accelerated_inferencing_device()
+
+    @property
+    def effective_precision(self):
+        """Return the precision mode selected for the currently loaded model."""
+        if len(getattr(self, "model_filenames", ())) > 1:
+            return FP32
+        return getattr(self.model_instance, "effective_precision", FP32)
+
+    @property
+    def effective_torch_compile(self):
+        """Return whether regional compilation is active for the loaded model."""
+        if len(getattr(self, "model_filenames", ())) > 1:
+            return False
+        return bool(getattr(self.model_instance, "effective_torch_compile", False))
 
     VALID_ENSEMBLE_ALGORITHMS = [
         "avg_wave", "median_wave", "min_wave", "max_wave",
@@ -835,10 +860,17 @@ class Separator:
 
         return model_data
 
-    def load_model(self, model_filename="model_bs_roformer_ep_317_sdr_12.9755.ckpt"):
+    def load_model(self, model_filename="model_bs_roformer_ep_317_sdr_12.9755.ckpt", force_reload=False):
         """
         This method instantiates the architecture-specific separation class,
         loading the separation model into memory, downloading it first if necessary.
+
+        Consecutive calls with the same single model reuse the loaded instance. Set
+        ``force_reload`` to ``True`` after changing settings captured at load time.
+
+        Args:
+            model_filename (str or list): The model filename, or filenames for an ensemble.
+            force_reload (bool): Reload a matching single model instead of reusing it.
         """
         # If an ensemble preset was loaded and no explicit model list was provided, use preset models
         if self._ensemble_preset_models is not None and model_filename == "model_bs_roformer_ep_317_sdr_12.9755.ckpt":
@@ -854,6 +886,10 @@ class Separator:
 
         self.model_filename = model_filename
         self.model_filenames = [model_filename]
+
+        if not force_reload and self.model_instance is not None and self._loaded_model_filename == model_filename:
+            self.logger.info(f"Model {model_filename} is already loaded; reusing the existing instance.")
+            return
 
         self.logger.info(f"Loading model {model_filename}...")
 
@@ -891,6 +927,9 @@ class Separator:
             "invert_using_spec": self.invert_using_spec,
             "sample_rate": self.sample_rate,
             "use_soundfile": self.use_soundfile,
+            "use_autocast": self.use_autocast,
+            "use_native_fp16": self.use_native_fp16,
+            "use_torch_compile": self.use_torch_compile,
         }
 
         # Instantiate the appropriate separator class depending on the model type
@@ -929,6 +968,12 @@ class Separator:
                 raise RuntimeError(error_msg) from e
             else:
                 raise
+
+        resolve_policy = getattr(self.model_instance, "resolve_execution_policy", None)
+        if callable(resolve_policy) and not getattr(self.model_instance, "_execution_policy_resolved", False):
+            resolve_policy(model_type.lower())
+
+        self._loaded_model_filename = model_filename
 
         # Log Roformer implementation version if applicable
         if hasattr(self.model_instance, 'is_roformer_model') and self.model_instance.is_roformer_model:
@@ -1027,21 +1072,45 @@ class Separator:
         self.logger.debug(f"Normalization threshold set to {self.normalization_threshold}, waveform will be lowered to this max amplitude to avoid clipping.")
         self.logger.debug(f"Amplification threshold set to {self.amplification_threshold}, waveform will be scaled up to this max amplitude if below it.")
 
-        # Run separation method for the loaded model with autocast enabled if supported by the device
+        # Run separation using the policy resolved for the loaded model's actual
+        # inference device. This matters when an architecture falls back to CPU.
         output_files = None
-        if self.use_autocast and autocast_mode.is_autocast_available(self.torch_device.type):
-            self.logger.debug("Autocast available.")
-            with autocast_mode.autocast(self.torch_device.type):
+        effective_precision = self.effective_precision
+        inference_device = getattr(self.model_instance, "torch_device", self.torch_device)
+        inference_device_type = getattr(inference_device, "type", str(inference_device))
+        separation_failed = False
+        try:
+            if effective_precision == NATIVE_FP16:
+                self.logger.debug("Using native float16 inference.")
                 output_files = self.model_instance.separate(audio_file_path, custom_output_names)
-        else:
-            self.logger.debug("Autocast unavailable.")
-            output_files = self.model_instance.separate(audio_file_path, custom_output_names)
-
-        # Clear GPU cache to free up memory
-        self.model_instance.clear_gpu_cache()
-
-        # Unset separation parameters to prevent accidentally re-using the wrong source files or output paths
-        self.model_instance.clear_file_specific_paths()
+            elif effective_precision == AUTOCAST and inference_device_type != "privateuseone":
+                self.logger.debug("Using autocast inference on %s.", inference_device_type)
+                with autocast_mode.autocast(inference_device_type):
+                    output_files = self.model_instance.separate(audio_file_path, custom_output_names)
+            else:
+                self.logger.debug("Using float32 inference.")
+                output_files = self.model_instance.separate(audio_file_path, custom_output_names)
+        except BaseException:
+            separation_failed = True
+            raise
+        finally:
+            # Reused instances must not retain per-file state after a failed inference.
+            cleanup_error = None
+            for cleanup_name in ("clear_gpu_cache", "clear_file_specific_paths"):
+                try:
+                    getattr(self.model_instance, cleanup_name)()
+                except Exception as exc:
+                    if separation_failed:
+                        self.logger.warning(
+                            "Cleanup %s failed after separation raised an error: %s",
+                            cleanup_name,
+                            exc,
+                            exc_info=True,
+                        )
+                    elif cleanup_error is None:
+                        cleanup_error = exc
+            if cleanup_error is not None:
+                raise cleanup_error
 
         # Remind the user one more time if they used a VIP model, so the message doesn't get lost in the logs
         self.print_uvr_vip_message()

nomadkaraoke/python-audio-separator

audio_separator/separator/architectures/demucs_separator.py
@@ -8,6 +8,7 @@ from audio_separator.separator.uvr_lib_v5.demucs.apply import apply_model, demuc
 from audio_separator.separator.uvr_lib_v5.demucs.hdemucs import HDemucs
 from audio_separator.separator.uvr_lib_v5.demucs.pretrained import get_model as get_demucs_model
 from audio_separator.separator.uvr_lib_v5 import spec_utils
+from audio_separator.separator.uvr_lib_v5.device_utils import should_accumulate_on_device
 
 DEMUCS_4_SOURCE = ["drums", "bass", "other", "vocals"]
 
@@ -21,6 +22,17 @@ DEMUCS_6_SOURCE_MAPPER = {
     CommonSeparator.GUITAR_STEM: 4,
     CommonSeparator.PIANO_STEM: 5,
 }
+
+
+def _estimate_demucs_full_track_buffer_bytes(channels, samples, num_sources, shifts, num_bag_models):
+    """Estimate peak float32 storage retained across Demucs split and shift passes."""
+    input_bytes = channels * samples * 4
+    output_copies = 2 if shifts > 1 else 1
+    if num_bag_models:
+        output_copies = max(output_copies, 2)
+        if num_bag_models > 1 and shifts > 1:
+            output_copies = 3
+    return 3 * input_bytes + output_copies * num_sources * input_bytes + samples * 4
 
 
 class DemucsSeparator(CommonSeparator):
@@ -107,19 +119,34 @@ class DemucsSeparator(CommonSeparator):
 
         self.logger.debug("Loading model for demixing...")
 
-        self.demucs_model_instance = HDemucs(sources=DEMUCS_4_SOURCE)
-        self.demucs_model_instance = get_demucs_model(name=os.path.splitext(os.path.basename(self.model_path))[0], repo=Path(os.path.dirname(self.model_path)))
-        self.demucs_model_instance = demucs_segments(self.segment_size, self.demucs_model_instance)
-        self.demucs_model_instance.to(self.torch_device)
-        self.demucs_model_instance.eval()
+        separation_failed = False
+        try:
+            self.demucs_model_instance = HDemucs(sources=DEMUCS_4_SOURCE)
+            self.demucs_model_instance = get_demucs_model(name=os.path.splitext(os.path.basename(self.model_path))[0], repo=Path(os.path.dirname(self.model_path)))
+            self.demucs_model_instance = demucs_segments(self.segment_size, self.demucs_model_instance)
+            self.demucs_model_instance.to(self.torch_device)
+            self.demucs_model_instance.eval()
 
-        self.logger.debug("Model loaded and set to evaluation mode.")
+            self.logger.debug("Model loaded and set to evaluation mode.")
 
-        source = self.demix_demucs(mix)
-
-        del self.demucs_model_instance
-        self.clear_gpu_cache()
-        self.logger.debug("Model and GPU cache cleared after demixing.")
+            source = self.demix_demucs(mix)
+        except BaseException:
+            separation_failed = True
+            raise
+        finally:
+            if hasattr(self, "demucs_model_instance"):
+                del self.demucs_model_instance
+            try:
+                self.clear_gpu_cache()
+            except Exception as cleanup_error:
+                if not separation_failed:
+                    raise
+                self.logger.warning(
+                    "Failed to clear the GPU cache after Demucs inference failed: %s",
+                    cleanup_error,
+                    exc_info=True,
+                )
+            self.logger.debug("Model and GPU cache cleared after demixing.")
 
         output_files = []
         self.logger.debug("Processing output files...")
@@ -166,14 +193,29 @@ class DemucsSeparator(CommonSeparator):
         self.logger.debug("Starting demixing process in demix_demucs...")
 
         processed = {}
-        mix = torch.tensor(mix, dtype=torch.float32)
+        num_sources = len(self.demucs_model_instance.sources)
+        estimated_buffer_bytes = _estimate_demucs_full_track_buffer_bytes(
+            channels=mix.shape[0],
+            samples=mix.shape[-1],
+            num_sources=num_sources,
+            shifts=self.shifts,
+            num_bag_models=len(getattr(self.demucs_model_instance, "models", ())),
+        )
+        accumulate_on_device = should_accumulate_on_device(self.torch_device, estimated_buffer_bytes)
+        mix_device = self.torch_device if accumulate_on_device else torch.device("cpu")
+        if self.torch_device.type == "mps" and not accumulate_on_device:
+            self.logger.info(
+                "Keeping full-track Demucs buffers on CPU for this long input to limit MPS memory use "
+                f"(estimated buffers: {estimated_buffer_bytes / 1024**3:.2f} GiB)."
+            )
+        mix = torch.tensor(mix, dtype=torch.float32, device=mix_device)
         ref = mix.mean(0)
         ref_mean = ref.mean()
         ref_std = ref.std()
         if not torch.isfinite(ref_std):
             ref_std = torch.zeros_like(ref_std)
         normalization_std = ref_std.clamp_min(torch.finfo(mix.dtype).eps)
-        mix = (mix - ref_mean) / normalization_std
+        mix.sub_(ref_mean).div_(normalization_std)
         mix_infer = mix
 
         with torch.no_grad():
@@ -190,7 +232,8 @@ class DemucsSeparator(CommonSeparator):
                 progress=True,
             )[0]
 
-        sources = (sources * ref_std + ref_mean).cpu().numpy()
+        sources.mul_(ref_std).add_(ref_mean)
+        sources = sources.cpu().numpy()
         sources[[0, 1]] = sources[[1, 0]]
         processed[mix] = sources[:, :, 0:None].copy()
         sources = list(processed.values())

audio_separator/separator/separator.py
@@ -26,6 +26,7 @@ from tqdm import tqdm
 from audio_separator.separator.audio_io import atomic_output_path, validate_audio_source
 from audio_separator.separator.ensembler import Ensembler
 from audio_separator.separator.exceptions import AudioExportError, BatchSeparationError, InvalidAudioDataError
+from audio_separator.separator.execution_policy import AUTOCAST, FP32, NATIVE_FP16
 
 # Mapping of common stem name variations to canonical names for ensemble grouping.
 STEM_NAME_MAP = {
@@ -84,7 +85,9 @@ class Separator:
         invert_using_spec (bool): Flag to invert using spectrogram.
         sample_rate (int): The sample rate of the audio.
         use_soundfile (bool): Use soundfile for audio writing, can solve OOM issues.
-        use_autocast (bool): Flag to use PyTorch autocast for faster inference.
+        use_autocast (bool): Use PyTorch autocast when the loaded model and device support it.
+        use_torch_compile (bool): Compile verified repeated model blocks when supported.
+        use_native_fp16 (bool): Convert a verified model to native float16 inference when supported.
 
     MDX Architecture Specific Attributes:
         hop_length (int): The hop length for STFT.
@@ -141,8 +144,13 @@ class Separator:
         ensemble_weights=None,
         ensemble_preset=None,
         info_only=False,
+        use_torch_compile=False,
+        use_native_fp16=False,
     ):
         """Initialize the separator."""
+        if use_autocast and use_native_fp16:
+            raise ValueError("use_autocast and use_native_fp16 are mutually exclusive precision modes.")
+
         self.logger = logging.getLogger(__name__)
         self.logger.setLevel(log_level)
         self.log_level = log_level
@@ -222,6 +230,8 @@ class Separator:
 
         self.use_soundfile = use_soundfile
         self.use_autocast = use_autocast
+        self.use_native_fp16 = use_native_fp16
+        self.use_torch_compile = use_torch_compile
         self.use_directml = use_directml
 
         self.chunk_duration = chunk_duration
@@ -260,12 +270,27 @@ class Separator:
         self.model_instance = None
         self.model_filename = None
         self.model_filenames = []
+        self._loaded_model_filename = None
 
         self.model_is_uvr_vip = False
         self.model_friendly_name = None
 
         if not info_only:
             self.setup_accelerated_inferencing_device()
+
+    @property
+    def effective_precision(self):
+        """Return the precision mode selected for the currently loaded model."""
+        if len(getattr(self, "model_filenames", ())) > 1:
+            return FP32
+        return getattr(self.model_instance, "effective_precision", FP32)
+
+    @property
+    def effective_torch_compile(self):
+        """Return whether regional compilation is active for the loaded model."""
+        if len(getattr(self, "model_filenames", ())) > 1:
+            return False
+        return bool(getattr(self.model_instance, "effective_torch_compile", False))
 
     VALID_ENSEMBLE_ALGORITHMS = [
         "avg_wave", "median_wave", "min_wave", "max_wave",
@@ -868,10 +893,17 @@ class Separator:
 
         return model_data
 
-    def load_model(self, model_filename="model_bs_roformer_ep_317_sdr_12.9755.ckpt"):
+    def load_model(self, model_filename="model_bs_roformer_ep_317_sdr_12.9755.ckpt", force_reload=False):
         """
         This method instantiates the architecture-specific separation class,
         loading the separation model into memory, downloading it first if necessary.
+
+        Consecutive calls with the same single model reuse the loaded instance. Set
+        ``force_reload`` to ``True`` after changing settings captured at load time.
+
+        Args:
+            model_filename (str or list): The model filename, or filenames for an ensemble.
+            force_reload (bool): Reload a matching single model instead of reusing it.
         """
         # If an ensemble preset was loaded and no explicit model list was provided, use preset models
         if self._ensemble_preset_models is not None and model_filename == "model_bs_roformer_ep_317_sdr_12.9755.ckpt":
@@ -887,6 +919,10 @@ class Separator:
 
         self.model_filename = model_filename
         self.model_filenames = [model_filename]
+
+        if not force_reload and self.model_instance is not None and self._loaded_model_filename == model_filename:
+            self.logger.info(f"Model {model_filename} is already loaded; reusing the existing instance.")
+            return
 
         self.logger.info(f"Loading model {model_filename}...")
 
@@ -924,6 +960,9 @@ class Separator:
             "invert_using_spec": self.invert_using_spec,
             "sample_rate": self.sample_rate,
             "use_soundfile": self.use_soundfile,
+            "use_autocast": self.use_autocast,
+            "use_native_fp16": self.use_native_fp16,
+            "use_torch_compile": self.use_torch_compile,
         }
 
         # Instantiate the appropriate separator class depending on the model type
@@ -962,6 +1001,12 @@ class Separator:
                 raise RuntimeError(error_msg) from e
             else:
                 raise
+
+        resolve_policy = getattr(self.model_instance, "resolve_execution_policy", None)
+        if callable(resolve_policy) and not getattr(self.model_instance, "_execution_policy_resolved", False):
+            resolve_policy(model_type.lower())
+
+        self._loaded_model_filename = model_filename
 
         # Log Roformer implementation version if applicable
         if hasattr(self.model_instance, 'is_roformer_model') and self.model_instance.is_roformer_model:
@@ -1097,15 +1142,22 @@ class Separator:
         self.logger.debug(f"Normalization threshold set to {self.normalization_threshold}, waveform will be lowered to this max amplitude to avoid clipping.")
         self.logger.debug(f"Amplification threshold set to {self.amplification_threshold}, waveform will be scaled up to this max amplitude if below it.")
 
+        # Run separation using the policy resolved for the loaded model's actual
+        # inference device. This matters when an architecture falls back to CPU.
+        output_files = None
+        effective_precision = self.effective_precision
+        inference_device = getattr(self.model_instance, "torch_device", self.torch_device)
+        inference_device_type = getattr(inference_device, "type", str(inference_device))
         try:
-            # Run separation method for the loaded model with autocast enabled if supported by the device
-            output_files = None
-            if self.use_autocast and autocast_mode.is_autocast_available(self.torch_device.type):
-                self.logger.debug("Autocast available.")
-                with autocast_mode.autocast(self.torch_device.type):
+            if effective_precision == NATIVE_FP16:
+                self.logger.debug("Using native float16 inference.")
+                output_files = self.model_instance.separate(audio_file_path, custom_output_names)
+            elif effective_precision == AUTOCAST and inference_device_type != "privateuseone":
+                self.logger.debug("Using autocast inference on %s.", inference_device_type)
+                with autocast_mode.autocast(inference_device_type):
                     output_files = self.model_instance.separate(audio_file_path, custom_output_names)
             else:
-                self.logger.debug("Autocast unavailable.")
+                self.logger.debug("Using float32 inference.")
                 output_files = self.model_instance.separate(audio_file_path, custom_output_names)
         finally:
             # Clear per-file state even when inference or audio export fails.